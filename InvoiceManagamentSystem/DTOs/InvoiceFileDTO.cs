75bcb0dd87c708a730487b38eaf533644d98d64e
JorgeMoragaCalvo <102338768+JorgeMoragaCalvo@users.noreply.github.com>
2026-02-12 15:44:27 -0300

    Delete InvoiceManagamentSystem directory

--- InvoiceManagamentSystem/DTOs/InvoiceFileDTO.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace InvoiceManagementSystem.DTOs;
-
-public class InvoiceFileDTO
-{
-    [JsonPropertyName("invoices")]
-    public List<InvoiceDTO> Invoices { get; set; } = new();
-}
