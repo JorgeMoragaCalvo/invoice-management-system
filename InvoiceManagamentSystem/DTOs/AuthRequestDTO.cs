e5908d2e9ec697e66b65f83bc2ca2b81cadb349a
JorgeMoragaCalvo <102338768+JorgeMoragaCalvo@users.noreply.github.com>
2026-02-12 12:56:45 +0000

    DTOs

---
+++ InvoiceManagamentSystem/DTOs/AuthRequestDTO.cs
@@ -0,0 +1,8 @@
+namespace InvoiceManagamentSystem.DTOs
+{
+    public class AuthRequestDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
